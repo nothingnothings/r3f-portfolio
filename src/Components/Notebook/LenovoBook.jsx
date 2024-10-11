495749bff779ed8af6df2c99d2b15a8af89efda7
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-10-11 11:19:15 -0300

    basic cleanup

--- src/Components/Notebook/LenovoBook.jsx
+++ src/Components/Notebook/LenovoBook.jsx
@@ -1,11 +1,9 @@
 import { meshBounds } from '@react-three/drei';
 
-import { useExplode } from '../../hooks/useFloat';
 import { useRef } from 'react';
 
 export default function LenovoBook({ nodes, materials, refName }) {
-  const groupRef = useRef();
-  const secondRef = useRef();
+
 
   return (
     <mesh
@@ -19,7 +17,6 @@ export default function LenovoBook({ nodes, materials, refName }) {
     >
       <group
         name="Top"
-        ref={secondRef}
         position={[0.007, -0.472, -10.412]}
         rotation={[1.358, 0, 0]}
         scale={5.796}
